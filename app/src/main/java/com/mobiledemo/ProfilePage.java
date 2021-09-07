e45801dca16f822d60eb10a6fbdc32e9cebcd983
Ziyao Ren <zren1996813@hotmail.com>
2021-09-07 11:23:11 -0400

    profile page display

--- app/src/main/java/com/mobiledemo/ProfilePage.java
+++ app/src/main/java/com/mobiledemo/ProfilePage.java
@@ -1,14 +1,54 @@
 package com.mobiledemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfilePage extends AppCompatActivity {
-
+    private String uid;
+    private FirebaseUser user;
+    private DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        uid = user.getUid();
+
+        final TextView nickNameField = (TextView) findViewById(R.id.nickNameField);
+        final TextView emailField = (TextView) findViewById(R.id.emailField);
+
+        reference.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if (userProfile != null) {
+                    String nickName = userProfile.nickName;
+                    String email = userProfile.email;
+                    nickNameField.setText(nickName);
+                    emailField.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfilePage.this, "Wrong", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
