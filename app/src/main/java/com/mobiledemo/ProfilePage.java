07c6c84296e6fa7c42adb4d0e3e8123f14940e52
zhangzhe <zhangz87@uw.edu>
2021-09-12 17:02:23 -0400

    search user feature

--- app/src/main/java/com/mobiledemo/ProfilePage.java
+++ app/src/main/java/com/mobiledemo/ProfilePage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
@@ -30,6 +32,7 @@ public class ProfilePage extends AppCompatActivity implements View.OnClickListen
     private EditText editMoment, editSearchUser;
     private Button editTextButton, searchUserButton;
     private TextView searchedUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,6 +101,51 @@ public class ProfilePage extends AppCompatActivity implements View.OnClickListen
     }
 
     public void searchUser() {
-        // TODO: 9/12/21 try to search for user 
+        // Do something in response to button
+        EditText editText = (EditText) findViewById(R.id.textEditSearchUser);
+        String targetName = editText.getText().toString();
+        final TextView searchedUserInfo = (TextView) findViewById(R.id.searchedUserInfo);
+        final TextView searchedEmail = (TextView) findViewById(R.id.searchedEmail);
+        final TextView searchedMoment = (TextView) findViewById(R.id.searchedMoment);
+
+        DatabaseReference reference;
+        final String[] targetEmail = new String[1];
+        final String[] targetMoment = new String[1];
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        Query query = reference.child("Users").orderByChild("nickName").equalTo(targetName);
+        query.addValueEventListener(new ValueEventListener() {
+            private static final String TAG = "No such Nickname";
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    User target = data.getValue(User.class);
+                    targetEmail[0] = target.email;
+                    targetMoment[0] = target.moment; // moment null check?
+                }
+                // display the result to the user
+                if (targetEmail[0] == null) {
+                    searchedUserInfo.setText("No such user");
+                } else {
+                    searchedUserInfo.setText(targetName);
+                    searchedEmail.setText(targetEmail[0]);
+                    if (targetMoment[0] == null) {
+                        searchedMoment.setText("No moment currently");
+                    } else {
+                        searchedMoment.setText(targetMoment[0]);
+                    }
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Toast.makeText(ProfilePage.this, "No such user", Toast.LENGTH_LONG).show();
+                // ...
+            }
+        });
     }
 }
--- app/src/main/java/com/mobiledemo/User.java
+++ app/src/main/java/com/mobiledemo/User.java
@@ -11,4 +11,5 @@ public class User {
         this.email = email;
     }
 
+
 }
