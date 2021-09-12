aaa9eca9eb0526d0c5a103c3fa8632e6d78654cc
Ziyao Ren <zren1996813@hotmail.com>
2021-09-12 14:09:45 -0500

    Add moment field. add search field. Needs to implement search function

--- app/src/main/java/com/mobiledemo/ProfilePage.java
+++ app/src/main/java/com/mobiledemo/ProfilePage.java
@@ -3,10 +3,16 @@ package com.mobiledemo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,10 +21,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ProfilePage extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class ProfilePage extends AppCompatActivity implements View.OnClickListener {
     private String uid;
     private FirebaseUser user;
     private DatabaseReference reference;
+    private EditText editMoment, editSearchUser;
+    private Button editTextButton, searchUserButton;
+    private TextView searchedUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +41,9 @@ public class ProfilePage extends AppCompatActivity {
 
         final TextView nickNameField = (TextView) findViewById(R.id.nickNameField);
         final TextView emailField = (TextView) findViewById(R.id.emailField);
+        editMoment = (EditText) findViewById(R.id.editTextMoment);
+        editTextButton = (Button) findViewById(R.id.editMomentButton);
+        editTextButton.setOnClickListener(this);
 
         reference.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -40,6 +54,8 @@ public class ProfilePage extends AppCompatActivity {
                     String email = userProfile.email;
                     nickNameField.setText(nickName);
                     emailField.setText(email);
+                    String moment = userProfile.moment;
+                    editMoment.setText(moment);
                 }
             }
 
@@ -49,6 +65,39 @@ public class ProfilePage extends AppCompatActivity {
             }
         });
 
+        editSearchUser = (EditText) findViewById(R.id.textEditSearchUser);
+        searchedUser = (TextView) findViewById(R.id.searchedUserInfo);
+        searchUserButton = (Button) findViewById(R.id.searchUserButton);
+        searchUserButton.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.editMomentButton:
+                updateMoment();
+            case R.id.searchUserButton:
+                searchUser();
+        }
+    }
+
+    public void updateMoment() {
+        System.out.println(editMoment.getText());
+        reference.child(uid).child("moment").setValue(editMoment.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ProfilePage.this, "Update Successful", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ProfilePage.this, "Update Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void searchUser() {
+        // TODO: 9/12/21 try to search for user 
+    }
 }
--- app/src/main/java/com/mobiledemo/User.java
+++ app/src/main/java/com/mobiledemo/User.java
@@ -1,7 +1,7 @@
 package com.mobiledemo;
 
 public class User {
-    public String nickName, email;
+    public String nickName, email, moment;
 
     public User() {
 
